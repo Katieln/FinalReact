8c79249b3ef17e1ef4b9f86906b0c5b75fd4e1d5
Katieln <139283838+Katieln@users.noreply.github.com>
2023-12-09 06:12:36 -0500

    firebase

--- src/components/ListContainer.jsx
+++ src/components/ListContainer.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 
 import Container from "react-bootstrap/Container";
 
+import { getFirestore, getDoc, doc, collection, getDocs } from "firebase/firestore";
+
 import { products } from "../data/products";
 import { ItemList } from "../components/ItemList";
 
@@ -10,9 +12,32 @@ export const ListContainer = (props) => {
     const [items, setItems] = useState([]);
 
     const { id } = useParams();
-    
+
+   /*  traer un solo doc o item de firebase
+        useEffect(() => {
+        const db = getFirestore();
+        const refDoc = doc(db, "Items", "UaVSsR0WKwIx4f3xkoMI");
+        getDoc(refDoc).then((snapshot) => {
+            console.log({ id: snapshot.id, ...snapshot.data()});
+        });
+    }, []); */
 
     useEffect(() => {
+        const db = getFirestore();
+        const refCollection = collection(db, "Items");
+        getDocs(refCollection).then((snapshot) => {
+            if (snapshot.size === 0) console.log ("No Results 🥺");
+            else
+            setItems(
+             snapshot.docs.map((doc) => {
+                return { id: doc.id, ...doc.data(), };
+             })
+             );
+        });
+    }, []);
+    
+
+   /*  useEffect(() => {
         const mypromise = new Promise ((resolve, reject) => {
             setTimeout (() => {
                 resolve(products);
@@ -28,10 +53,10 @@ export const ListContainer = (props) => {
                 setItems(filterByCategory);
             }
         });
-    }, [id]);
-
+    }, [id]); */
+/* 
     console.log(items);
-
+ */
     return (
     <Container className="mt-4">
         <h1>{props.greeting}</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { initializeApp } from "firebase/app";
 import App from './App.jsx'
 
 
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNziiSGG6psnaec0wWFXV0RKtuuBZn6MY",
+  authDomain: "webviajes-ebcca.firebaseapp.com",
+  projectId: "webviajes-ebcca",
+  storageBucket: "webviajes-ebcca.appspot.com",
+  messagingSenderId: "225926310746",
+  appId: "1:225926310746:web:aac64776717c65c176e163"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
